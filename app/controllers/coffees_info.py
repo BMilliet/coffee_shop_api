4eb98036d68f7006c9f15080e6a6951f6bc5648c
Bruno <brunomilliet89@gmail.com>
2019-12-18 10:16:21 -0300

    adjusts

--- app/controllers/coffees_info.py
+++ app/controllers/coffees_info.py
@@ -4,15 +4,15 @@ from flask import jsonify
 coffees = [{
     'name': 'espresso',
     'type': 1,
-    'price': 3.50
+    'price': 1.50
 }, {
     'name': 'double espresso',
     'type': 1,
-    'price': 6.00
+    'price': 3.00
 }, {
     'name': 'cappuccino',
     'type': 2,
-    'price': 6.00
+    'price': 3.00
 }]
 
 

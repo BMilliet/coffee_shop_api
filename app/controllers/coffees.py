2a084258b39c2ba54c3e768bd82bafb706df4ee6
Bruno <brunomilliet89@gmail.com>
2020-01-01 15:48:11 -0300

    add to db and class methods

--- app/controllers/coffees.py
+++ app/controllers/coffees.py
@@ -1,7 +1,8 @@
 from app import app
 from typing import List, Dict
-from flask import jsonify
-from app.services import db_manager, db_query
+from flask import jsonify, request
+from app.models import coffee
+from app.services import db_manager, query_manager
 import mysql.connector
 import json
 
@@ -17,16 +18,27 @@ def api_info() -> Dict:
 
 
 def all_coffees() -> List[Dict]:
-    results = db_manager.requestDB(db_manager.dbQuery, db_query.SELECTALL)
+    query = query_manager.select_all()
+    results = db_manager.requestDB(db_manager.dbQuery, query)
     return results
 
 
 def coffees_id(id) -> List[Dict]:
-    results = db_manager.requestDB(db_manager.dbQuery,
-                                   db_query.SELECTWHEREID + str(id))
+    query = query_manager.select_where(str(id))
+    results = db_manager.requestDB(db_manager.dbQuery, query)
     return results
 
 
 def delete_from_id(id) -> str:
-    db_manager.requestDB(db_manager.dbChange, db_query.DELETEWHEREID + str(id))
+    query = query_manager.delete_where(str(id))
+    db_manager.requestDB(db_manager.dbChange, query)
     return 'delete coffee with id ' + str(id)
+
+
+def insert_item() -> str:
+    data = request.get_json(force=True)
+    coffee_object = coffee.coffee_from_json(data)
+    values = coffee_object.to_db_values()
+    query = query_manager.insert_new(values)
+    db_manager.requestDB(db_manager.dbChange, query)
+    return 'added new coffee'
--- app/routes/coffees.py
+++ app/routes/coffees.py
@@ -9,21 +9,26 @@ def welcome():
     return jsonify(controller.api_info()), 200
 
 
-@app.route('/hello')
-def hello():
+@app.route('/hello', methods=['GET'])
+def hello() -> str:
     return 'Hello World!'
 
 
-@app.route('/allCoffees')
+@app.route('/allCoffees', methods=['GET'])
 def index() -> str:
     return json.dumps({'results': controller.all_coffees()})
 
 
-@app.route('/CoffeeId/<int:coffee_id>')
+@app.route('/CoffeeId/<int:coffee_id>', methods=['GET'])
 def coffee_by_id(coffee_id) -> str:
     return json.dumps({'result': controller.coffees_id(coffee_id)})
 
 
-@app.route('/DeleteWhereId/<int:coffee_id>')
+@app.route('/DeleteWhereId/<int:coffee_id>', methods=['GET'])
 def delete_where_id(coffee_id) -> str:
     return json.dumps({'result': controller.delete_from_id(coffee_id)})
+
+
+@app.route('/AddNew', methods=['POST'])
+def incert_new_item() -> str:
+    return controller.insert_item()

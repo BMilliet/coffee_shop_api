dcfac2e2eba005883e01021dbaa84c9409c05288
Bruno <brunomilliet89@gmail.com>
2020-01-01 16:08:31 -0300

    refactor text

--- db/init.sql
+++ db/init.sql
@@ -14,5 +14,5 @@ VALUES
   (0, 'Espresso', 'Espresso is brewed by using an espresso machine to force a small amount of nearly boiling water and steam.'),
   (0, 'Cappucino', 'Cappuccino, from Italian, is an Italian drink prepared with espresso and milk.'),
   (0, 'French press', 'A French press requires a coarser grind of coffee than a drip brew coffee filter, as finer grounds will seep through the press filter and into the coffee.'),
-  (0, 'Mocaccino', 'Caffè mocha is based on espresso and hot milk but with added chocolate flavouring and sweetener, typically in the form of cocoa powder and sugar.')
+  (0, 'Mocaccino', 'Coffee mocha is based on espresso and hot milk but with added chocolate flavouring and sweetener, typically in the form of cocoa powder and sugar.')
 

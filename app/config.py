8f28f9f9c1ab5225dbd7537fc7999daf12c6d5fa
Bruno <brunomilliet89@gmail.com>
2020-01-09 15:49:03 -0300

    setup envs and db adjust

--- app/config.py
+++ app/config.py
@@ -1,15 +1,9 @@
 import os
 
-user = 'test'
-password = 'root'
-host = 'db'
-database = 'coffee_db'
-port = '5432'
-
-# user = os.environ['POSTGRES_USER']
-# password = os.environ['POSTGRES_PASSWORD']
-# host = os.environ['POSTGRES_HOST']
-# database = os.environ['POSTGRES_DB']
-# port = os.environ['POSTGRES_PORT']
+user = os.environ['POSTGRES_USER']
+password = os.environ['POSTGRES_PASSWORD']
+host = os.environ['POSTGRES_HOST']
+database = os.environ['POSTGRES_DB']
+port = os.environ['POSTGRES_PORT']
 
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
--- app/models.py
+++ app/models.py
@@ -7,5 +7,5 @@ class Coffee(db.Model):
     __tablename__ = 'coffee'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    price = db.Column(db.Integer)
+    price = db.Column(db.Numeric(asdecimal=False))
     details = db.Column(db.String(200))

c4520893d3e9e22f96082568ad4bb94983b8a5fc
Bruno <brunomilliet89@gmail.com>
2020-01-11 12:38:17 -0300

    object from json

--- app/serializer.py
+++ app/serializer.py
@@ -3,14 +3,20 @@ from .models import *
 
 
 def checkoutTojson(coffees) -> str:
-    total = sum(coffee['price'] for coffee in coffees)
+    cart = list(coffeeFromJson(coffee) for coffee in coffees)
+    total = sum(item.price for item in cart)
     checkout = {'cart': coffees, 'total': total}
     return json.dumps(checkout)
 
 
-# def coffeeFromJson() -> CoffeeModel:
-#     json = request.get_json(force=True)
-#     return CoffeeModel(name=json['name'], price=json['price'])
+def coffeeFromJson(json) -> CoffeeModel:
+    return CoffeeModel(name=json['name'], price=json['price'])
+
+
+# def receiptToJson() -> str:
+
+#     return json.dumps("")
+
 
 # def checkoutFromJson() -> Checkout:
 #     json = request.get_json(force=True)

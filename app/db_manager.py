82ffcc4a8d33ab8ec8de2b8ce1d3dd7c973b9629
Bruno <brunomilliet89@gmail.com>
2020-01-12 10:44:22 -0300

    new table and methods

--- app/db_manager.py
+++ app/db_manager.py
@@ -3,20 +3,34 @@ import json
 from flask import request
 
 from app import app, database
-from app.migration import Coffee
+from app.migration import Coffee, Receipt
 
 
-def fetchAll() -> list:
+def fetchAllCaffee() -> list:
     coffees = database.get_all(Coffee)
     return list(coffee.representation() for coffee in coffees)
 
 
-def add() -> str:
+def fetchAllReceipt() -> list:
+    receipts = database.get_all(Receipt)
+    return list(receipt.representation() for receipt in receipts)
+
+
+def addCoffee() -> str:
     data = request.get_json(force=True)
     database.add_instance(Coffee,
                           name=data['name'],
                           price=data['price'],
                           details=data['details'])
+
+
+def addReceipt() -> str:
+    data = request.get_json(force=True)
+    database.add_instance(Receipt,
+                          coffee_ids=list(
+                              item['id'] for item in data['checkout']['card']),
+                          total=data['checkout']['total'],
+                          payment=data['form_of_payment'])
 
 
 def remove(coffee_id):
